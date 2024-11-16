518d172628d26f72d4b365fe470511c8306ba77b
yulia <yuliia_yaresko@knu.ua>
2024-11-16 14:10:18 +0200

    Add project files.

---
+++ MauiApp3/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace MauiApp3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(NumberEntry.Text, out int number))
+            {
+                DisplayAlert("Повідомлення", $"Ви ввели число {number}", "OK");
+            }
+            else
+            {
+                DisplayAlert("Помилка", "Введене значення не є цілим числом.", "OK");
+            }
+        }
+    }
+}
